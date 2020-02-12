9751317e3059702f1baa1037b6a1b9714a8f9e48
drewnutt <anm329@pitt.edu>
2020-02-11 22:53:16 -0500

    fixed ensemble stuff

--- GNINA_RUN/check_ensembles.py
+++ GNINA_RUN/check_ensembles.py
@@ -4,7 +4,6 @@ import argparse
 
 parser= argparse.ArgumentParser()
 parser.add_argument('--trainf', required=True, help='training input file for DDG model')
-parser.add_argument('--vina', action='store_true', help='report classification using Vina affinity')
 parser.add_argument('--model', required=True, help='gnina model used to calculate info')
 args = parser.parse_args()
 
@@ -14,23 +13,21 @@ for i in range(4):
     j = i + 1
     gnina_ddg = 'gnina_DDG_{}_{}.txt'.format(args.model, j)
     model = pd.read_csv(gnina_ddg, sep=' ')
-    ensemble['labelcaff'] = ensemble['labelcaff'] + model['labelcaff']
-    ensemble['labelscr'] = ensemble['labelscr'] + model['labelscr']
+    ensemble['l_gnina_aff'] = ensemble['l_gnina_aff'] + model['l_gnina_aff']
+    ensemble['l_scr'] = ensemble['l_scr'] + model['l_scr']
 
-ensemble['labelcaff'] = ensemble['labelcaff']/5.0
-ensemble['labelscr'] = ensemble['labelscr']/5.0
-ensemble['labelcaff'] = ensemble['labelcaff'].round()
-ensemble['labelscr'] = ensemble['labelscr'].round()
+ensemble['l_gnina_aff'] = ensemble['l_gnina_aff']/5.0
+ensemble['l_scr'] = ensemble['l_scr']/5.0
+ensemble['l_gnina_aff'] = ensemble['l_gnina_aff'].round()
+ensemble['l_scr'] = ensemble['l_scr'].round()
 train_data = pd.read_csv(args.trainf, sep=' ', header=None, names=['label','diff','lig1','lig2'], usecols=[0,1,3,4])
 train_data['lig2'] = train_data['lig2'].apply(lambda x: x[5:11])
 train_data['lig1'] = train_data['lig1'].apply(lambda x: x[5:11])
 assert ensemble.shape[0] == train_data.shape[0]
-gnina_lbls = ensemble[['labelcaff','labelscr','labelvina']].copy()
+gnina_lbls = ensemble[['l_gnina_aff','l_scr']].copy()
 labels = train_data['label']
 comp_dict = gnina_lbls.eq(labels, axis=0).mean(axis=0, numeric_only=True).to_dict()
-output_string = '{}:\nMetric | Accuracy\n-----|-----\nGNINA Affinity | {}\nGNINA Score | {}'.format(args.model, comp_dict['labelcaff'],comp_dict['labelscr'])
-if args.vina:
-    output_string += 'Vina Affinity | {}'.format(comp_dict['labelvina'])
+output_string = '{}:\nMetric | Accuracy\n-----|-----\nGNINA Affinity | {}\nGNINA Score | {}'.format(args.model, comp_dict['l_gnina_aff'],comp_dict['l_scr'])
 model_stat = '{}_{}_stats.txt'.format('ensemble',args.model)
 with open(model_stat,'w') as f:
     f.write(output_string)
