3aa86e5606a29188af4b8cb99be464669a7fa30a
Drew McNutt <anm329@pitt.edu>
2021-06-08 17:47:11 -0400

    create a constant testing set thats useful for up to 6 ligs

--- python_files/select_subset.py
+++ python_files/select_subset.py
@@ -7,6 +7,7 @@ parser.add_argument('-T','--train',required=True,help='Train types file')
 parser.add_argument('-E','--test',required=True,help='Test types file')
 parser.add_argument('-N',required=True,type=int,help='number of test ligands to include in the train file')
 parser.add_argument('-S','--random_seed',default=42,type=int,help='random seed')
+parser.add_argument('--test_subset',action='store_true',default=False,help='keep a subset of the test data, will only work if N=6')
 args = parser.parse_args()
 
 def split_comparison(comparison,rng):
@@ -39,7 +40,12 @@ rng = np.random.default_rng(seed=args.random_seed)
 
 test_data = pd.read_table(args.test, sep=' ', header=None)
 test_data.columns = ['cls','ddg', 'dg1','dg2','rec','lig1','lig2']
-tt_comp = test_data.sample(n=1,random_state=args.random_seed)
+group_cts = test_data.value_counts(subset=['rec'])
+big_group = group_cts.index[0][0]
+num_ligs = (1+np.sqrt(1+4*group_cts[0]))/2
+print(f"{big_group}: {num_ligs}")
+series = test_data[test_data['rec'] == big_group].copy()
+tt_comp = series.sample(n=1, random_state=args.random_seed)
 n_left = args.N-2
 if args.N == 1:
     tt_comp = split_comparison(tt_comp,rng)
@@ -68,4 +74,8 @@ else:
             rec = comp['rec'].unique().item()
             n_left -= 2
 
+if args.N ==6 and args.test_subset:
+    leftover = series.drop(index=tt_comp.index)
+    if leftover is not None:
+        leftover.to_csv(f"{args.test.split('.')[0]}_TE.types", sep=' ' , header=False,index=False)
 tt_comp.to_csv(args.train,sep=' ',header=False,index=False,mode='a') 
