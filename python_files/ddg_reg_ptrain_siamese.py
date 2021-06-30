fix problem with rotation loss weight array being too small

python_files/ddg_reg_ptrain_siamese.py
@@ -685,7 +685,7 @@ if args.rot_warmup:
     before_warmup = np.full((init_iters,),0)
     num_iters = num_iters_pe * args.rot_warmup
     warmup_schedule = np.linspace(0,args.rotation_loss_weight,int(num_iters) )
-    final_iters = num_iters_pe * (args.epoch-args.rot_warmup-10)
+    final_iters = num_iters_pe * (args.epoch)
     args.rotation_loss_weight = np.concatenate((before_warmup,warmup_schedule,np.full((final_iters,),args.rotation_loss_weight)))
 else:
     all_iters = num_iters_pe * (args.epoch)